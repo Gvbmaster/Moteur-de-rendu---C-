ba2d57803ea72a2e2b899ab956535b3aee42db2a
AlphaQGC <alegrix@gaming.tech>
2023-11-03 04:00:39 +0100

    finit

--- Moteur de rendu - C++/GameObject.cpp
+++ Moteur de rendu - C++/GameObject.cpp
@@ -1,40 +1,39 @@
-#include "GameObject.h"
-#include "SDL.h"
-#include "SDL_image.h"
-
-#include <iostream>
-#include <iomanip>
-#include <cstdlib>
-#include <ctime>
-
-GameObject::GameObject(SDL_Renderer* renderer, const char* imagePath, int x, int y, int width, int height)
-    : renderer(renderer) {
-    // Chargez l'image depuis le chemin spécifié
-    SDL_Surface* surface = SDL_LoadBMP(imagePath);
-    if (surface) {
-        texture = SDL_CreateTextureFromSurface(renderer, surface);
-        SDL_FreeSurface(surface);
-
-        rect.x = x;
-        rect.y = y;
-        rect.w = width;
-        rect.h = height;
-    }
-    else {
-        std::cout << "error";
-    }
-        
-
-}
-
-GameObject::~GameObject() {
-    if (texture) {
-        SDL_DestroyTexture(texture);
-    }
-}
-
-void GameObject::render() {
-    if (texture) {
-        SDL_RenderCopy(renderer, texture, NULL, &rect);
-    }
-}
+//#include "GameObject.h"
+//#include "SDL.h"
+//
+//#include <iostream>
+//#include <iomanip>
+//#include <cstdlib>
+//#include <ctime>
+//
+//GameObject::GameObject(SDL_Renderer* renderer, const char* imagePath, int x, int y, int width, int height)
+//    : renderer(renderer) {
+//    // Chargez l'image depuis le chemin spécifié
+//    SDL_Surface* surface = SDL_LoadBMP(imagePath);
+//    if (surface) {
+//        texture = SDL_CreateTextureFromSurface(renderer, surface);
+//        SDL_FreeSurface(surface);
+//
+//        rect.x = x;
+//        rect.y = y;
+//        rect.w = width;
+//        rect.h = height;
+//    }
+//    else {
+//        std::cout << "error";
+//    }
+//        
+//
+//}
+//
+//GameObject::~GameObject() {
+//    if (texture) {
+//        SDL_DestroyTexture(texture);
+//    }
+//}
+//
+//void GameObject::render() {
+//    if (texture) {
+//        SDL_RenderCopy(renderer, texture, NULL, &rect);
+//    }
+//}
--- Moteur de rendu - C++/Moteur de rendu - C++.cpp
+++ Moteur de rendu - C++/Moteur de rendu - C++.cpp
@@ -1,36 +1,62 @@
-// Moteur de rendu - C++.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
-
 #include <iostream>
-#include "Window.h"
-#include "GameObject.h"
-#include "SDL.h"
-#include "SDL_image.h"
+#include <SDL.h>
 
 int main(int argc, char* argv[]) {
-    Window window("Ma fenêtre SDL", 800, 600);
+    const int SCREEN_WIDTH = 800;
+    const int SCREEN_HEIGHT = 800;
 
-    GameObject object(window.getRenderer(), "panda.bmp", 100, 100, 50, 50);
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
 
-    bool running = true;
+    SDL_Window* window = SDL_CreateWindow("2048 Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == nullptr) {
+        std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 2;
+    }
 
-    while (running) {
-        SDL_Event event;
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                running = false;
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == nullptr) {
+        std::cerr << "Renderer could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 3;
+    }
+
+    SDL_Surface* pandaImage = SDL_LoadBMP("img/panda.bmp");
+    if (pandaImage == nullptr) {
+        std::cerr << "Image could not be loaded! SDL_Error: " << SDL_GetError() << std::endl;
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 4;
+    }
+
+    SDL_Texture* pandaTexture = SDL_CreateTextureFromSurface(renderer, pandaImage);
+    SDL_FreeSurface(pandaImage);
+
+    SDL_Rect destRect = { 200, 200, 200, 200 };
+
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, pandaTexture, NULL, &destRect);
+    SDL_RenderPresent(renderer);
+
+    SDL_Event e;
+    bool quit = false;
+    while (!quit) {
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) {
+                quit = true;
             }
         }
-
-        window.clear();
-
-        // Affichez l'objet
-        object.render();
-
-        window.render();
     }
+
+    SDL_DestroyTexture(pandaTexture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
-
-
